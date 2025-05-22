3767f3ab0c7d33fc52fa816f2afb0f0322f0b367
Venish krishna K <95084701+venishkrishna@users.noreply.github.com>
2025-05-22 18:43:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Venish123@localhost/DatingAppDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:####'
 
 engine =  create_engine(SQLALCHEMY_DATABASE_URL)
 
